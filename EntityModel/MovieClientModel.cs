112f76c7fa604835cda72619975924ec2899ff3c
Ebin <ebinipe@gmail.com>
2017-12-28 12:46:47 +0530

    Add files via upload

---
+++ EntityModel/MovieClientModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieBuff.Models
+{  
+public class MovieClientModel
+    {
+    public int MovieID { get; set; }
+    public string MovieName { get; set; }
+    public int YOR { get; set; }
+    public int ProducerID { get; set; }
+        public string Producer { get; set; }
+
+        public string ImgSrcString { get; set; }
+        public List<string> lstActors { get; set; }
+}
+}
